2e89c7059b4490707b889a1e85ad9f3c6ca0951f
kiko <kiko@comunitea.com>
2017-01-10 23:13:39 +0100

    [IMP] Modulo hr_apl. enlazar hr con tareas. Campo code en empleado

---
+++ project-addons/apl/apl_hr/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# © 2016 Comunitea Servicios Tecnologicos (<http://www.comunitea.com>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+
+{
+    'name': 'Modulo APL HR',
+    'version': '10.0',
+    'author': 'Comunitea, ',
+    "category": "",
+    'license': 'AGPL-3',
+    'description': 'Modulo con cambios necesarios para integraciond e hr_employee',
+    'depends': [
+        'base', 'hr'
+    ],
+    'contributors': [
+        "Comunitea",
+        "Kiko Sanchez<kiko@comunitea.com>",    ],
+    "data": [
+        'views/hr_employee.xml',
+    ],
+    "demo": [
+
+    ],
+    'test': [
+
+    ],
+    'installable': True
+}
---
+++ project-addons/apl/apl_hr/models/hr_employee.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © 2016 Comunitea Servicios Tecnologicos (<http://www.comunitea.com>)
+# Kiko Sanchez (<kiko@comunitea.com>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+
+from odoo import fields, models, tools, api, _
+
+from odoo.exceptions import UserError, ValidationError
+from datetime import datetime, timedelta
+
+class Employee(models.Model):
+
+    _inherit = "hr.employee"
+
+    employee_code = fields.Char("Codigo", help ="Codigo de empleado")
